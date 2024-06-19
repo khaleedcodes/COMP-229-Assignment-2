96d2635f0c81a9ab33d062063e712efef3d225ef
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-06-18 23:53:33 -0400

    will try to fix mondodb not returning anything, connection works tho

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -9,11 +9,24 @@ exports.create = (req, res) => {
 };
 
 // Retrieve all Products
+// exports.findAll = (req, res) => {
+//     Product.find()
+//         .then(products => res.json(products))
+//         .catch(err => res.status(500).json({ message: err.message }));
+// };
 exports.findAll = (req, res) => {
-    Product.find()
-        .then(products => res.json(products))
-        .catch(err => res.status(500).json({ message: err.message }));
+  console.log("findAll request received");
+  Product.find()
+      .then(products => {
+          console.log("Products found: ", products);
+          res.json(products);
+      })
+      .catch(err => {
+          console.error("Error finding products: ", err);
+          res.status(500).json({ message: err.message });
+      });
 };
+
 
 // Retrieve a single Product by ID
 exports.findOne = (req, res) => {
